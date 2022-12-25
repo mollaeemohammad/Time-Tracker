6f3aace3f6d1181062371bf8b281272a3171f19a
Saeed Reza <sr.amanat80@gmail.com>
2022-12-25 16:48:34 +0330

    Routing: removed hash, added nested routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,31 @@
 // import Vue from "vue";
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import AlertVue from "./../components/AlertVue.vue";
 import SignupEmployee from "./../components/SignupEmployee.vue";
 import SignupEmployer from "./../components/SignupEmployer.vue";
 import LoginEmployee from "./../components/LoginEmployee.vue";
 import LoginEmployer from "./../components/LoginEmployer.vue";
 
-// Vue.use(VueRouter);
-
 const routes = [
     { path: "/", component: AlertVue },
-    { path: "/employee/signup", component: SignupEmployee },
-    { path: "/employer/signup", component: SignupEmployer },
-    { path: "/employee/login", component: LoginEmployee },
-    { path: "/employer/login", component: LoginEmployer },
+    {
+        path: "/employee",
+        children: [
+            { path: "signup", component: SignupEmployee },
+            { path: "login", component: LoginEmployee },
+        ],
+    },
+    {
+        path: "/employer",
+        children: [
+            { path: "signup", component: SignupEmployer },
+            { path: "login", component: LoginEmployer },
+        ],
+    },
 ];
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
 });
 
