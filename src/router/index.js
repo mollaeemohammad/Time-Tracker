2c7c5962da1bb4a4c94140d62dccaa3c6e65720f
Saeed Reza <sr.amanat80@gmail.com>
2023-01-06 11:30:17 +0330

    Some changes in routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,37 @@
 // import Vue from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import AlertVue from "./../components/AlertVue.vue";
-import SignupEmployee from "./../components/SignupEmployee.vue";
-import SignupEmployer from "./../components/SignupEmployer.vue";
-import LoginEmployee from "./../components/LoginEmployee.vue";
-import LoginEmployer from "./../components/LoginEmployer.vue";
+import SignupForm from "./../components/signupForm.vue";
+import LoginForm from "./../components/loginForm.vue";
+import HomeEmployer from "./../components/HomeEmployer.vue";
+import ProjectRegister from "./../components/ProjectRegister.vue";
+import Test from "./../components/Test.vue";
+import Test2 from "./../components/Test2.vue";
+import Test3 from "./../components/Test3.vue";
 
 const routes = [
     { path: "/", component: AlertVue },
+    { path: "/signup", name: "signup", component: SignupForm },
+    { path: "/login", name: "login", component: LoginForm },
     {
-        path: "/employee",
-        children: [
-            { path: "signup", component: SignupEmployee },
-            { path: "login", component: LoginEmployee },
-        ],
+        path: "/home",
+        name: "home",
+        component: HomeEmployer,
+        children: [],
     },
     {
-        path: "/employer",
-        children: [
-            { path: "signup", component: SignupEmployer },
-            { path: "login", component: LoginEmployer },
-        ],
+        path: "/project",
+        name: "home-project",
+        component: ProjectRegister,
     },
+
+    { path: "/test", name: "test", component: Test },
+    {
+        path: "/test2",
+        name: "test2",
+        component: Test2,
+    },
+    { path: "/test3", name: "test3", component: Test3 },
 ];
 
 const router = createRouter({
