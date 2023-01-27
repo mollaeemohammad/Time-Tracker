863f1fb51ac2f70f37c4376a9ce3e3d79b5d9759
Saeed Reza <sr.amanat80@gmail.com>
2023-01-28 01:59:58 +0330

    Router updated

--- src/router/index.js
+++ src/router/index.js
@@ -4,34 +4,80 @@ import AlertVue from "./../components/AlertVue.vue";
 import SignupForm from "./../components/signupForm.vue";
 import LoginForm from "./../components/loginForm.vue";
 import HomeEmployer from "./../components/HomeEmployer.vue";
+import HomeEmployee from "./../components/HomeEmployee.vue";
+import ReportEmployee from "./../components/ReportEmployee.vue";
+import ProfileEmployee from "./../components/ProfileEmployee.vue";
 import ProjectRegister from "./../components/ProjectRegister.vue";
+import ProjectSelect from "./../components/ProjectSelect.vue";
+import BottomNav from "./../components/BottomNav.vue";
 import Test from "./../components/Test.vue";
 import Test2 from "./../components/Test2.vue";
 import Test3 from "./../components/Test3.vue";
+import Test4 from "./../components/Test4.vue";
+import TestChart from "./../components/TestChart.vue";
 
 const routes = [
-    { path: "/", component: AlertVue },
+    { path: "/", name: "main", component: AlertVue },
     { path: "/signup", name: "signup", component: SignupForm },
     { path: "/login", name: "login", component: LoginForm },
-    {
-        path: "/home",
-        name: "home",
-        component: HomeEmployer,
-        children: [],
-    },
-    {
-        path: "/project",
-        name: "home-project",
-        component: ProjectRegister,
-    },
 
     { path: "/test", name: "test", component: Test },
     {
-        path: "/test2",
-        name: "test2",
-        component: Test2,
+        path: "/app",
+        name: "app",
+        component: BottomNav,
+        children: [
+            {
+                path: "employer/home",
+                name: "homeEmployer",
+                component: HomeEmployer,
+            },
+            {
+                path: "employer/project/:projectName?",
+                name: "projectEmployer",
+                component: ProjectRegister,
+            },
+            {
+                path: "employer/select",
+                name: "selectProjectEmployer",
+                component: ProjectSelect,
+            },
+
+            {
+                path: "employer/profile",
+                name: "profileEmployer",
+                component: ProfileEmployee,
+            },
+
+            {
+                path: "employee/home",
+                name: "homeEmployee",
+                component: HomeEmployee,
+            },
+            // {
+            //     path: "employee/profile",
+            //     name: "profileEmployee",
+            //     component: ProfileEmployee,
+            // },
+            {
+                path: "employee/profile",
+                name: "profileEmployee",
+                component: ProfileEmployee,
+            },
+            {
+                path: "employee/report",
+                name: "reportEmployee",
+                component: ReportEmployee,
+            },
+            {
+                path: "employer/report",
+                name: "reportEmployer",
+                component: ReportEmployee,
+            },
+        ],
     },
     { path: "/test3", name: "test3", component: Test3 },
+    { path: "/chart", name: "chart", component: TestChart },
 ];
 
 const router = createRouter({
